751cdb4725524397f12dc1f73fe2af11c21c3c1c
Pritam Sethuraman <78743757+Pritam-Sethuraman@users.noreply.github.com>
2021-08-11 11:41:55 +0530

    Create 2D Convolution.py

---
+++ 2D Convolution.py
@@ -0,0 +1,77 @@
+# Importing Necessary Libraries
+import skimage.data
+import numpy
+import matplotlib
+import sys
+
+def conv_(img, conv_filter):
+    filter_size = conv_filter.shape[1]
+    result = numpy.zeros((img.shape))
+    #Looping through the image to apply the convolution operation.
+    for r in numpy.uint16(numpy.arange(filter_size/2.0, img.shape[0]-filter_size/2.0+1)):
+        for c in numpy.uint16(numpy.arange(filter_size/2.0, img.shape[1]-filter_size/2.0+1)):
+            curr_region = img[r-numpy.uint16(numpy.floor(filter_size/2.0)):r+numpy.uint16(numpy.ceil(filter_size/2.0)), 
+                              c-numpy.uint16(numpy.floor(filter_size/2.0)):c+numpy.uint16(numpy.ceil(filter_size/2.0))]
+            #Element-wise multipliplication between the current region and the filter.
+            curr_result = curr_region * conv_filter
+            conv_sum = numpy.sum(curr_result) #Summing the result of multiplication.
+            result[r, c] = conv_sum #Saving the summation in the convolution layer feature map.
+            
+    #Clipping the outliers of the result matrix.
+    final_result = result[numpy.uint16(filter_size/2.0):result.shape[0]-numpy.uint16(filter_size/2.0), 
+                          numpy.uint16(filter_size/2.0):result.shape[1]-numpy.uint16(filter_size/2.0)]
+    return final_result
+
+def conv(img, conv_filter):
+
+    if len(img.shape) != len(conv_filter.shape) - 1: # Check whether number of dimensions is the same
+        print("Error: Number of dimensions in conv filter and image do not match.")  
+        exit()
+    if len(img.shape) > 2 or len(conv_filter.shape) > 3: # Check if number of image channels matches the filter depth.
+        if img.shape[-1] != conv_filter.shape[-1]:
+            print("Error: Number of channels in both image and filter must match.")
+            sys.exit()
+    if conv_filter.shape[1] != conv_filter.shape[2]: # Check if filter dimensions are equal.
+        print('Error: Filter must be a square matrix. I.e. number of rows and columns must match.')
+        sys.exit()
+    if conv_filter.shape[1]%2==0: # Check if filter diemnsions are odd.
+        print('Error: Filter must have an odd size. I.e. number of rows and columns must be odd.')
+        sys.exit()
+
+    # An empty feature map to hold the output of convolving the filter(s) with the image.
+    feature_maps = numpy.zeros((img.shape[0]-conv_filter.shape[1]+1, img.shape[1]-conv_filter.shape[1]+1, conv_filter.shape[0]))
+
+    # Convolving the image by the filter(s).
+    for filter_num in range(conv_filter.shape[0]):
+        print("Filter ", filter_num + 1)
+        curr_filter = conv_filter[filter_num, :] # getting a filter from the bank.
+        if len(curr_filter.shape) > 2:
+            conv_map = conv_(img[:, :, 0], curr_filter[:, :, 0]) # Array holding the sum of all feature maps.
+            for ch_num in range(1, curr_filter.shape[-1]): # Convolving each channel with the image and summing the results.
+                conv_map = conv_map + conv_(img[:, :, ch_num], 
+                                  curr_filter[:, :, ch_num])
+        else: # There is just a single channel in the filter.
+            conv_map = conv_(img, curr_filter)
+        feature_maps[:, :, filter_num] = conv_map # Holding feature map with the current filter.
+    return feature_maps # Returning all feature maps.
+
+
+# Reading the image
+img = skimage.data.chelsea()
+#img = skimage.data.camera()
+
+# Converting the image into gray.
+img = skimage.color.rgb2gray(img)
+
+# First conv layer
+#l1_filter = numpy.random.rand(2,7,7)*20 # Preparing the filters randomly.
+l1_filter = numpy.zeros((2,3,3))
+l1_filter[0, :, :] = numpy.array([[[-1, 0, 1], 
+                                   [-1, 0, 1], 
+                                   [-1, 0, 1]]])
+l1_filter[1, :, :] = numpy.array([[[1,   1,  1], 
+                                   [0,   0,  0], 
+                                   [-1, -1, -1]]])
+
+print("\n**Working with conv layer 1**")
+%prun l1_feature_map = conv(img, l1_filter)
